819d469e91a7da9ea5936b445d9a056df003f72f
Oleksandr Sidarok <sasha.sidarok@disroot.org>
2022-08-11 12:42:03 +0300

    add section professional-washing for HomePage

--- src/components/UI/AppButton.vue
+++ src/components/UI/AppButton.vue
@@ -1,15 +1,49 @@
 <template>
-  <button class="button">
+  <button class="button" :class="props.decoration" v-if="props.type === 'button'" :type="props.type">
     <slot />
   </button>
+  <a :href="`${linkType}${link}`" class="button" v-else>
+    <slot />
+  </a>
 </template>
+
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: "button"
+  },
+  link: {
+    type: String,
+    default: ""
+  },
+  decoration: {
+    type: String,
+    default: "fill"
+  }
+})
+
+let linkType = computed(() => {
+  switch (props.type) {
+    case "tel":
+        return "tel:"
+  }
+})
+</script>
 
 <style scoped>
   .button {
-    padding: 20px 0;
+    padding: 20px 30px;
     background: var(--accent-color);
-    box-shadow: inset 0px -7px 7px #FF013E;
     border-radius: 10px;
+    border: 1px solid transparent;
     color: white;
+  }
+
+  .outline {
+    background: transparent;
+    border: 1px solid #ffffff;
   }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,12 +2,64 @@
   <section>
     <the-intro />
   </section>
+  <section class="professional-washing">
+    <div class="container">
+      <div class="inner">
+        <div class="content">
+          <h2 class="title">Professional Washing And Cleaning.</h2>
+          <div class="text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+              mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+            </p>
+          </div>
+          <div class="buttons-wrapper">
+            <app-button type="tel" link="+910000000000">+91-0000-000000</app-button>
+            <app-button decoration="outline">Learn More</app-button>
+          </div>
+        </div>
+        <div class="image">
+          <img src="../assets/img/profession-washing.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
 import TheIntro from "../components/TheIntro.vue";
+import AppButton from "../components/UI/AppButton.vue"
 </script>
 
 <style scoped>
+.professional-washing {
+  padding: 90px 0;
+  background: var(--background-color);
+}
 
+.professional-washing .inner {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.professional-washing .content {
+  width: 50%;
+}
+
+.professional-washing .image {
+  width: 45%;
+}
+
+.image img {
+  width: 100%;
+  height: auto;
+}
+
+.buttons-wrapper {
+  display: flex;
+  gap: 20px;
+  margin-top: 50px;
+}
 </style>
